32c7d8f1b5f72849534997056ae5d9757e7f11ad
ChristIsEnough <oluwakamiyeayayandele01@gmail.com>
2022-06-25 16:55:54 +0100

    first commit to intoduction to js

---
+++ introductionto.js
@@ -0,0 +1,15 @@
+// const myName =(prompt('My name is Oluwakamiye. I am from NIGERIA. I am about 5 to 6 feet tall'));
+// console.log(myName);
+
+// const country = (prompt('I am from Nigeria.'));
+// console.log(country);
+
+// const height = (prompt('I am about 5.9 feet tall'));
+// console.log(height);
+
+// using get innerhtml to display my name,height and country.
+
+document.getElementById("introto-js").innerHTML = "My name is Oluwakamiye Ayandele. I'm from Nigeria, I have an height 5'9 feet.";
+
+
+
